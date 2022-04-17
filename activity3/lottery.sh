ffccacab0494a653bae48389a5c4f36092c36348
EC2 Default User <ec2-user@ip-172-31-43-154.eu-west-2.compute.internal>
2022-04-17 16:18:43 +0000

    Activity 3

--- activity3/lottery.sh
+++ activity3/lottery.sh
@@ -11,23 +11,22 @@ fi
 
 while true
 do
-	num=`echo $((1 + RANDOM % 50))`
-	grep -w $num lottery.$DATE > /dev/null
-	exit=$(echo $?)
-	if [[ ($exit == 0)]]
-	then
-		continue
-	else
-		echo $num >> lottery.$DATE
-		ball=$(wc -l lottery.$DATE | awk '{print$1}')
-		if [ $ball -gt 4 ]
-		then
-			bonusball=`echo $((1 + RANDOM % 10))`  
-			echo $bonusball > bonusball.$DATE
-			break
-		else
-			continue
-		fi
-	fi
+        num=`echo $((1 + RANDOM % 50))`
+        grep -w $num lottery.$DATE > /dev/null
+        exit=$(echo $?)
+        if [[ ($exit == 0)]]
+        then
+                continue
+        else
+                echo $num >> lottery.$DATE
+                ball=$(wc -l lottery.$DATE | awk '{print$1}')
+                if [ $ball -gt 4 ]
+                then
+                        bonusball=`echo $((1 + RANDOM % 10))`
+                        echo $bonusball > bonusball.$DATE
+                        break
+                else
+                        continue
+                fi
+        fi
 done
-
