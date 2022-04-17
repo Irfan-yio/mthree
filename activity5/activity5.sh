6c628b7f2bc21442a3c8732c94238d500ed65971
EC2 Default User <ec2-user@ip-172-31-43-154.eu-west-2.compute.internal>
2022-04-17 15:21:31 +0000

    Activity 5

--- activity5/activity5.sh
+++ activity5/activity5.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
+function numchecker()
+{
+        echo $input | grep [0-9] > /dev/null
+        exit=$(echo $?)
+        echo $input | grep [a-z] > /dev/null
+        exit2=$(echo $?)
+        echo $input | grep [A-Z] > /dev/null
+        exit3=$(echo $?)
+}
+
 while true
 do
-	read -p "Please enter a number: " number
-	echo $number | grep [0-9] > /dev/null
-	exit=$(echo $?)
-	echo $number | grep [a-z] > /dev/null
-	exit2=$(echo $?)
-	echo $number | grep [A-Z] > /dev/null
-        exit3=$(echo $?)
+	read -p "Please enter a number: " input
+	numchecker
 	if [[ ($exit == 0 && $exit2 != 0 && $exit3 != 0) ]]
 	then
-		echo $number | rev
+		echo $input | rev
 		break
 	else
 		echo "That's not a number..."
