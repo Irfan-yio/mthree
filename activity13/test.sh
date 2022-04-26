aa849939abf37167aa1918b728c4021379d88a44
EC2 Default User <ec2-user@ip-172-31-43-154.eu-west-2.compute.internal>
2022-04-26 09:36:18 +0000

    Activity 13

---
+++ activity13/test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+read -p "Please enter the name of the first file: " file1
+until [ -f "$file1" ]
+do
+        echo "Invalid input. File does not exit."
+        read -p "Please enter the name of the first file: " file1
+done
+read -p "Please enter the name of the second file: " file2
+until [ -f "$file2" ]
+do
+        echo "Invalid input. File does not exit."
+        read -p "Please enter the name of the second file: " file2
+done
+echo "Only in file 1: "
+awk 'NR==FNR{lines[$0];next}!($0 in lines)' $file2 $file1
+echo "Only in file 2: "
+awk 'NR==FNR{lines[$0];next}!($0 in lines)' $file1 $file2
